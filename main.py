210007041f2a276ad83e242886bd99dc6dfe8023
mic <micmor.m@gmail.com>
2021-05-10 01:05:04 -0400

    Create ASCII picture

--- main.py
+++ main.py
@@ -1,6 +1,64 @@
 import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+end_of_game = False
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+lives = 6
 
 print(f'Pssst, the solution is {chosen_word}.')
 
@@ -8,7 +66,7 @@ display = []
 for ch in chosen_word:
     display.append("_")
 
-while "_" in display:
+while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
     i = 0
@@ -16,7 +74,20 @@ while "_" in display:
         if ch == guess:
             display[i] = guess
         i += 1
+    if guess not in chosen_word:
+        print(stages[lives])
+        lives -= 1
 
-    print(display)
+    # Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
 
-print("You win!")
+    # Check if user has got all letters.
+    if lives < 0:
+        end_of_game = True
+        print("You lose.")
+
+    # Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
